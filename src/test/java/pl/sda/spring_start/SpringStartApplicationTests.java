8eea625d05d3edd4ba6d7d8ac48ef10b4ebb5331
MikiKru <michal_kruczkowski@o2.pl>
2020-11-07 09:47:50 +0100

    spring v0

---
+++ src/test/java/pl/sda/spring_start/SpringStartApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.spring_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
