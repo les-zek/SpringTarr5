a38a23ae78e84e12bf31611fc557ea7d5e72e28e
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-11-08 13:07:14 +0100

    Spring training blog application with theme 8

--- src/main/java/pl/sda/spring_start/controller/BlogController.java
+++ src/main/java/pl/sda/spring_start/controller/BlogController.java
@@ -1,12 +1,15 @@
 package pl.sda.spring_start.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogController {
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("title", "Leszek's blog");
+        model.addAttribute("subtitle", "Most stupid blog ever");
         return "index";
     }
 }
