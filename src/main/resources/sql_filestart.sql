0ed98848025cc4fa5965535e1ac77a237b7b9efb
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-11-14 16:20:04 +0100

    Spring training blog application with theme 21

--- src/main/resources/sql_filestart.sql
+++ src/main/resources/sql_filestart.sql
@@ -13,3 +13,18 @@ select * from post;
 
 SELECT p.category, count(*) FROM Post p GROUP BY p.category ORDER BY 2 DESC;
 select * from user;
+select * from role;
+
+select u.email, u.password, u.status from user u where u.email ="aa@aa.aa";
+SELECT
+	u.email,
+    r.role_name
+FROM
+	user u
+		JOIN
+	user_roles ur ON (u.user_id = ur.user_user_id)
+		JOIN
+	role r ON (r.role_id = ur.roles_role_id)
+WHERE
+	u.email = ?;
+    
